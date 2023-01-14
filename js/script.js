922a12996eecea70f94f18f6f7aae99ed64153f5
dzauroff <120591845+dzauroff@users.noreply.github.com>
2023-01-14 14:48:33 +0300

    lambo

--- js/script.js
+++ js/script.js
@@ -1,3 +1,15 @@
+const burgerBtn = document.querySelector('.burger');
+const menuClose = document.querySelector('.menu-close');
+const menuBurger = document.querySelector('.nav');
+
+burgerBtn.addEventListener('click', () => {
+    menuBurger.classList.add('burger-active');
+});
+
+menuClose.addEventListener('click', () => {
+    menuBurger.classList.remove('burger-active');
+});
+
 const swiper = new Swiper('.swiper', {
     loop: true,
     slidesPerView: 2,
@@ -7,5 +19,5 @@ const swiper = new Swiper('.swiper', {
       nextEl: '.swiper-button-right',
       prevEl: '.swiper-button-left',
     },
-  
+   
   });
